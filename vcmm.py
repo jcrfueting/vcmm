7071e7931b714383bf64f6831c10366cfbef26bd
Janos <jcr.fueting@gmail.com>
2023-11-23 22:17:51 -0500

    initial code skeleton

---
+++ vcmm.py
@@ -0,0 +1,164 @@
+###
+###   Vine Copula Mixture Models
+###
+
+import numpy as np
+import pyvinecopulib as vcl
+
+### functions
+
+def loglike(data, F, gamma, V, pi, r):
+    """
+    log-likelihood of vine copula mixture
+    """
+    llh = 0
+    return llh
+
+def initial_prob(data, K, method):
+    """
+    initial cluster assignment probabilities in VCMM algorithm
+
+    Input:
+        data: NxD np array of data
+        K: number of groups
+        method: string specifying the fast clustering method
+
+    Output:
+        z: NxK np array of assignment probabilities
+    """
+    # TODO
+    z = np.arange()
+    return z
+  
+def select_marginals(data, r):
+    return families, parameters, u
+
+def select_tree_structure(u, copulas, trunclevel=1):
+    return trees
+  
+def select_copulas(V, u):
+    return families, parameters
+
+def RVineStructureSelect(u, copulas, trunclevel=1):
+    V = select_tree_structure(u=u, copulas=copulas, trunclevel=trunclevel)
+    families, parameters = select_copulas(V, u)
+    return V, families, parameters
+
+def posterior_prob(data, pi, chi, psi):
+    """
+    E-step
+
+    Input:
+        data: NxD np array of data
+        pi: 
+        chi: 
+        psi
+
+    Output:
+        z: NxK np array of assignment probabilities
+    """
+    return z
+
+def mixture_weights(r):
+    """
+    CM-step 1
+
+    Input:
+        r: posterior cluster probabilities
+
+    Output:
+        pi: 1xK np array of mixture weights (soft group proportions)
+    """
+    return pi
+  
+def marginal_parameters(r, data, F):
+    """
+    CM-step 2
+
+    Input:
+      r: NxK array of cluster probabilites
+      data: NxD array with data on original scale
+      F: KxD array with families of marginal distributions
+    Output:
+        gamma: KxD structure of parameters (some parameters might be vectors)
+    """
+    return gamma
+  
+def pair_copula_parameters(r, data, F, gamma, V):
+    """
+    CM-step 3
+
+    Input:
+
+    Output:
+        theta: structure of pair copula parameters (some parameters might be vectors)
+    """
+    return theta
+  
+
+  
+
+
+def vcmm(data, K, tol=0.00001, maxiter=100, initial_method, fitting_trunclevel=1):
+  
+    n, d = data.shape
+    
+    # 1. initial assignment
+    r = initial_prob(data, K, method=initial_method)
+    
+    # 2. initial model selection
+    F, gamma, u = select_marginals(data, r)
+    V, families, parameters = RVineStructureSelect(u, copulas, trunclevel=truncation_level)
+    
+    # 3. parameter estimation
+    t = 0
+    while True :
+      llh_old = llh
+      r_old = r
+      gamma_old = gamma
+      theta_old = theta
+      
+      # E step
+      r = posterior_prob()
+      
+      # CM step 1
+      pi = mixture_weights(r)
+      
+      # CM step 2
+      gamma = marginal_parameters(r, data, F)
+      
+      # CM step 3
+      theta = pair_copula_parameters(r, data, F, gamma, V)
+      
+      # stopping condition(s)
+      llh = loglik(data, r_old, gamma_old, theta_old)
+      llhdiff = (llh-llh_old)/llh_old
+      if llhdiff < tol :
+          break
+      if t >= maxiter :
+          break
+        
+    # 4. temporary cluster assignment
+    c = r.argmax(axis=1)
+    
+    # 5. final model selection
+    F, gamma, u = select_marginals(data, r)
+    V, families, parameters = RVineStructureSelect(u, copulas, trunclevel=d-1)
+    
+    # 6. final cluster assignment
+    c = posterior_prob().argmax(axis=1)
+    
+    # classifier
+    def predict(x):
+        pred_prob = posterior_prob()
+        return pred_prob.argmax(axis=1), pred_prob
+    
+    return c, F, gamma, V, B, theta, pi, r, predict
+  
+
+### simulation study
+
+
+### applications
+
+
